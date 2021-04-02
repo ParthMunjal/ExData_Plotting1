2834c5919946bdae49b0ad97f19ea154d4ff3bfa
ParthMunjal <pmunjal.2000@gmail.com>
2021-04-02 18:37:07 +0530

    commit for the first plot

---
+++ plot_1.R
@@ -0,0 +1,6 @@
+df <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?")
+data <- subset(df, Date %in% c("1/2/2007","2/2/2007"))
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+hist(data$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
